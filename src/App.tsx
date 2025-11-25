2fe5ed8a45270e5ee97d7119f8ad57a090814925
Jorge CL <jorge.cl31@gmail.com>
2025-11-25 15:32:11 +0000

    Fix: Cambio a HashRouter para compatibilidad con GitHub Pages

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,12 @@ import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import Servicios from "./pages/Servicios";
 import Productos from "./pages/Productos";
-import Beneficios from "./pages/Servicios";
 import Clientes from "./pages/Clientes";
 import Contacto from "./pages/Contacto";
-import NotFound from "./pages/NotFound";
 
 const queryClient = new QueryClient();
 
@@ -18,18 +16,15 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <HashRouter>
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/servicios" element={<Servicios />} />
           <Route path="/productos" element={<Productos />} />
-          <Route path="/beneficios" element={<Beneficios />} />
           <Route path="/clientes" element={<Clientes />} />
           <Route path="/contacto" element={<Contacto />} />
-          {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
-          <Route path="*" element={<NotFound />} />
         </Routes>
-      </BrowserRouter>
+      </HashRouter>
     </TooltipProvider>
   </QueryClientProvider>
 );
